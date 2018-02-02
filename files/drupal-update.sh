f567b950f5dc0522ffb0aac994e283286acb7d56
Kristof Coomans <kristof@2dotstwice.be>
2018-02-02 12:07:13 +0100

    Don't use sudo for drush fra, more clear separation between D7 and D8

--- files/drupal-update.sh
+++ files/drupal-update.sh
@@ -3,22 +3,22 @@
 cd /usr/share/nginx/html;
 
 if [ ! -e ./core ]; then
-  sudo -u www-data drush fra --yes || true;
-fi
+  # Drupal 7
+  drush fra --yes || true;
 
-drush -y updb;
-drush -y entity-updates;
+  drush -y updb;
+  drush -y entity-updates;
 
-# Config sync
-if [ -e ./core ]; then
+   drush -y cc all;
+else
+  # Drupal 8
+  drush -y updb;
+  drush -y entity-updates;
+
   drush en -y config_split || true;
   drush cc drush || true;
   drush csim -y || true;
-fi
 
-# Final cache clear / rebuild
-if [ ! -e ./core ]; then
-  drush -y cc all;
-else
   drush -y cr;
 fi
+
